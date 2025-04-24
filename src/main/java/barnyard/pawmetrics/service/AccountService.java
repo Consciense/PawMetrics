a3d85f53ebb43321c3c00a09ac65ce5426a57bdd
Consciense <mr.jj921@gmail.com>
2025-04-24 19:16:42 +0300

    Added mapper

--- src/main/java/barnyard/pawmetrics/service/AccountService.java
+++ src/main/java/barnyard/pawmetrics/service/AccountService.java
@@ -1,11 +1,12 @@
 package barnyard.pawmetrics.service;
 
+import barnyard.pawmetrics.domain.dto.RegistrationDTO;
 import barnyard.pawmetrics.domain.entity.Account;
 import barnyard.pawmetrics.domain.model.Role;
+import barnyard.pawmetrics.mapper.AccountMapper;
 import barnyard.pawmetrics.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,37 +22,51 @@ public class AccountService implements UserDetailsService {
     @Autowired
     private AccountRepository repository;
     @Autowired
+    AccountMapper mapper;
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Transactional
-    public void create(Account user) {
-        if (repository.existsByUsername(user.getUsername())) {
-            throw new RuntimeException("Username already exists");
-        }
-
-        if (repository.existsByEmail(user.getEmail())) {
-            throw new RuntimeException("Email already exists");
-        }
+    public void create(RegistrationDTO dto) {
+        Account user = mapper.toEntity(dto);
         user.setAuthorities(Set.of(Role.USER));
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         repository.save(user);
     }
 
-    public Account getByUsername(String username) {
-        return repository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
-    }
+//    public Account getByUsername(String username) {
+//        return repository.findByUsername(username)
+//                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+//    }
 
-    public Account getCurrentUser() {
-        return getByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
-    }
+//    public Account getCurrentUser() {
+//        return getByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+//    }
+//
+//
+//    public boolean login(LoginDTO loginDTO) {
+//        if (repository.existsByUsernameAndPassword(loginDTO.getUsername(), loginDTO.getPassword())) {
+//
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    public Account edit(Account user) {
+//        Account currentUser = getCurrentUser();
+//        currentUser.setEmail(user.getEmail());
+//        currentUser.setUsername(user.getUsername());
+//        currentUser.setPassword(user.getPassword());
+//        currentUser.setPhoto(user.getPhoto());
+//        return repository.save(currentUser);
+//    }
 
-    @Deprecated
-    public void setAdmin() {
-        Account user = getCurrentUser();
-        user.setAuthorities(Set.of(Role.ADMIN));
-        repository.save(user);
-    }
+//    @Deprecated
+//    public void setAdmin() {
+//        Account user = getCurrentUser();
+//        user.setAuthorities(Set.of(Role.ADMIN));
+//        repository.save(user);
+//    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
